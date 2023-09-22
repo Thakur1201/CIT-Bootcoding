5352c6275edb322d1bc8f33cf4889ac24baa8edd
Aryan Thakur <144718346+Thakur1201@users.noreply.github.com>
2023-09-22 17:23:34 +0530

    Update Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
-public class Assignment{
+public class Test{
 public static void main(String [] args){
-int a;
+int a,b;
 Scanner s1=new Scanner(System.in);
 System.out.println("Enter First number:");
 a=s1.nextInt();
-a+=5;
-System.out.println("Value of a+= is: "+a);
+System.out.println("Value of a= is: "+a);
+System.out.println("Value of a+=5 is: "+(a+=5));
+System.out.println("Value of a-=5 is: "+(a-=5));
+System.out.println("Value of a*=5 is: "+(a*=5));
+System.out.println("Value of a/=5 is: "+(a/=5));
+System.out.println("Value of a%=5 is: "+(a%=5));
 }
 }
