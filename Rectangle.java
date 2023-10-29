37e5fb29e7a6144ac3c0e5b453a2644bce6e0064
Aryan Thakur <144718346+Thakur1201@users.noreply.github.com>
2023-10-30 00:13:15 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,23 @@
+package test;
+
+import java.util.Scanner;
+public class Rectangle implements Area,Perimeter{
+	static double length,breadth;
+	
+	public void perimeter() {
+		System.out.print("Perimeter of Rectangle is: "+(2*(length+breadth)));
+	}
+	public void area() {
+		System.out.print("\nArea of Rectangle is: "+(length*breadth));
+	}
+	public static void main(String[] args) {
+		Scanner s1=new Scanner(System.in);	
+		System.out.print("Enter value of Length: ");
+		length=s1.nextDouble();
+		System.out.print("Enter value of Breadth: ");
+		breadth=s1.nextDouble();
+		Rectangle r1=new Rectangle();
+		r1.perimeter();
+		r1.area();
+	}
+}
