e6927ea3f0f6e323e7ab56d4d7205b0519a6c2f4
Aryan Thakur <144718346+Thakur1201@users.noreply.github.com>
2023-10-30 15:51:25 +0530

    Update StudentDisplay.java

--- StudentDisplay.java
+++ StudentDisplay.java
@@ -7,7 +7,7 @@ public class StudentDisplay extends StudentMarks{
 		d1.inputMarks();
 		int total=m1+m2+m3+m4+m5;
 		float avg=total/5;
-		float per=(total/500)*100;
+		float per=100*total/500;
 		System.out.print("Total marks scored: "+total);
 		System.out.print("Average: "+avg);
 		System.out.print("Percentage: "+per);
